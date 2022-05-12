6eb6c200d9b09e87770a158e4aba74fa525d99a5
devxsby <devxsby@gmail.com>
2022-05-12 16:17:36 +0900

    [Feat] Home 스토리보드, 뷰컨트롤러 추가 (#1)

---
+++ E-NAME/E-NAME/Source/Screen/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  E-NAME
+//
+//  Created by subinyoon on 2022/05/12.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
