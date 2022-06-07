ce880cbc93610e8c43e8cb0695dee1ec6769a89c
devxsby <devxsby@gmail.com>
2022-06-07 17:58:30 +0900

    [Feat] #4- 홈 뷰 레이아웃 구현

--- E-NAME/E-NAME/Source/Screen/Home/HomeViewController.swift
+++ E-NAME/E-NAME/Source/Screen/Home/HomeViewController.swift
@@ -11,6 +11,18 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        backButtonCustom()
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        guard let loginVC = UIStoryboard(name: "Login", bundle: nil).instantiateViewController(withIdentifier: LoginViewController.className) as? LoginViewController else { return }
         
+        self.navigationController?.pushViewController(loginVC, animated: true)
+    }
+    
+    @IBAction func signupButtonTapped(_ sender: Any) {
+        guard let signupVC = UIStoryboard(name: "SignUp", bundle: nil).instantiateViewController(withIdentifier: SignUpViewController.className) as? SignUpViewController else { return }
+        
+        self.navigationController?.pushViewController(signupVC, animated: true)
     }
 }
