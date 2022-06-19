62b188cd06cb0e7417aa8ee59f1555954fc7ef88
devxsby <devxsby@gmail.com>
2022-06-20 04:38:32 +0900

    [Feat] 설문조사 뷰 구현

--- E-NAME/E-NAME/Source/Screen/Survey/ResultViewController.swift
+++ E-NAME/E-NAME/Source/Screen/Survey/ResultViewController.swift
@@ -8,13 +8,33 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    
+    var datas: [Data] = [] {
+        didSet {
+            print("mock data update")
+        }
+    }
+    
+    var nameData: Data?
+    var genderString = ""
+    
     @IBOutlet weak var detailView: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var detailLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setData()
         setTapGesture()
+        setUI()
+    }
+    
+    func setData() {
+        guard let data = MockParser.load([Data].self, from: "Data") else { return }
+        print(data.count)
+        let randomNum = Int.random(in: 0..<73)
+        print(randomNum)
+        nameData = data[randomNum]
     }
     
     func setTapGesture() {
@@ -24,11 +44,34 @@ class ResultViewController: UIViewController {
     
     @objc func handleTap(sender: UITapGestureRecognizer) {        
         guard let bottomVC = self.storyboard?.instantiateViewController(withIdentifier: "BottomViewController") as? BottomViewController else { return }
+        
+        bottomVC.name = nameData?.name
+        bottomVC.englishMeaning = nameData?.mean.components(separatedBy: "@")[0]
+        bottomVC.koreanMeaning = nameData?.mean.components(separatedBy: "@")[1]
+        bottomVC.genderMeaning = genderString
+
         bottomVC.modalTransitionStyle = .crossDissolve
         bottomVC.modalPresentationStyle = .overFullScreen
         present(bottomVC, animated: false) {
             bottomVC.showBottomSheetWithAnimation()
         }
+    }
+    
+    func setUI() {
+        backButtonCustom()
+        
+        switch nameData?.gender {
+        case 0:
+            genderString = "남성"
+        case 1:
+            genderString = "여성"
+        case 2:
+            genderString = "무관"
+        default:
+            break
+        }
+        nameLabel.text = nameData?.name
+        detailLabel.text = "뜻 : " + (nameData?.mean.replacingOccurrences(of: "@", with: "\n     ") ?? "") + "\n성별 : " + genderString
     }
     
     @IBAction func homeButtonTapped(_ sender: Any) {
--- E-NAME/E-NAME/Source/Screen/Survey/ThirdViewController.swift
+++ E-NAME/E-NAME/Source/Screen/Survey/ThirdViewController.swift
@@ -9,14 +9,56 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
+    var isFirstViewSelected: Bool = false
+    var submitCount = 0
+    var imageArray = [[UIImage(named: "B1"), UIImage(named: "B2")],
+                      [UIImage(named: "C1"), UIImage(named: "C2")],
+                      [UIImage(named: "D1"), UIImage(named: "D2")]]
+    
+    @IBOutlet weak var containerView1: UIView!
+    @IBOutlet weak var containerView2: UIView!
+    
+    @IBOutlet weak var imageLabel1: UILabel!
+    @IBOutlet weak var imageLabel2: UILabel!
+    
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        backButtonCustom()
+        setTapGesture()
     }
     
     @IBAction func nextButtonTapped(_ sender: Any) {
-        guard let nextVC = UIStoryboard(name: "Result", bundle: nil).instantiateViewController(withIdentifier: ResultViewController.className) as? ResultViewController else { return }
-        self.navigationController?.pushViewController(nextVC, animated: true)
+        submitCount += 1
+        if submitCount == 4 {
+            guard let nextVC = UIStoryboard(name: "Result", bundle: nil).instantiateViewController(withIdentifier: WaitingViewController.className) as? WaitingViewController else { return }
+            self.navigationController?.pushViewController(nextVC, animated: true)
+        } else {
+            switch submitCount {
+            case 1:
+                imageLabel1.text = "B - 1"
+                imageLabel2.text = "B - 2"
+                imageView1.image = imageArray[submitCount-1][0]
+                imageView2.image = imageArray[submitCount-1][1]
+            case 2:
+                imageLabel1.text = "C - 1"
+                imageLabel2.text = "C - 2"
+                imageView1.image = imageArray[submitCount-1][0]
+                imageView2.image = imageArray[submitCount-1][1]
+            case 3:
+                imageLabel1.text = "D - 1"
+                imageLabel2.text = "D - 2"
+                imageView1.image = imageArray[submitCount-1][0]
+                imageView2.image = imageArray[submitCount-1][1]
+            default:
+                break
+            }
+        }
+        containerView1.backgroundColor = UIColor(named: "enameLightGrey")
+        containerView2.backgroundColor = UIColor(named: "enameLightGrey")
+
     }
     
     @IBAction func cancelButtonTapped(_ sender: Any) {
@@ -28,4 +70,20 @@ class ThirdViewController: UIViewController {
         UIView.transition(with: uWindow, duration: 0.5, options: [.transitionCrossDissolve], animations: {}, completion: nil)
     }
     
+    func setTapGesture() {
+        let tapGesture1 = UITapGestureRecognizer(target: self, action: #selector(handleTap1(sender:)))
+        let tapGesture2 = UITapGestureRecognizer(target: self, action: #selector(handleTap2(sender:)))
+        containerView1.addGestureRecognizer(tapGesture1)
+        containerView2.addGestureRecognizer(tapGesture2)
+    }
+    
+    @objc func handleTap1(sender: UITapGestureRecognizer) {
+        sender.view?.backgroundColor = UIColor(named: "enameMediumGrey")
+        containerView2.backgroundColor = UIColor(named: "enameLightGrey")
+    }
+    
+    @objc func handleTap2(sender: UITapGestureRecognizer) {
+        sender.view?.backgroundColor = UIColor(named: "enameMediumGrey")
+        containerView1.backgroundColor = UIColor(named: "enameLightGrey")
+    }
 }
