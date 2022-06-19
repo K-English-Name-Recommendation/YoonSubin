2a50799f0e2e3db05e95591e748c1983bb45df9f
devxsby <devxsby@gmail.com>
2022-06-20 01:49:34 +0900

    [Feat] 프로필 스토리보드, 컨트롤러 파일 추가

---
+++ E-NAME/E-NAME/Source/Screen/Main/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  E-NAME
+//
+//  Created by subinyoon on 2022/06/19.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
